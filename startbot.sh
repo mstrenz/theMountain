a56649542f16f375cd4b8b64dd5cf50e9115501b
matt.strenz <matt.strenz@readytalk.com>
2016-07-06 07:43:55 -0600

    Using new slack token

--- startbot.sh
+++ startbot.sh
@@ -2,5 +2,5 @@
 
 export HUBOT_GOOGLE_CSE_ID="011019601835429903623:9q6jjlhnrwi"
 export HUBOT_GOOGLE_CSE_KEY="AIzaSyAgtCtI0HDNyouMubrtQ2zeSjINRbgf4PU"
-export HUBOT_SLACK_TOKEN="xoxb-56136324275-UUGatuRbcxmX5qzWt2h6XRn4"
+export HUBOT_SLACK_TOKEN="xoxb-56136324275-qfJjywJVMao2KQRsNjrHUWna"
 bin/hubot --adapter slack
